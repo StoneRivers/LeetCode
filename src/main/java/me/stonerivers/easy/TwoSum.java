f96cfa4acee16a8adcf6f8110e84cd3761bdf940
zhangyuanan <zhangyuanan@meituan.com>
2018-10-08 21:17:36 +0800

    first commit

---
+++ src/main/java/me/stonerivers/easy/TwoSum.java
@@ -0,0 +1,34 @@
+package me.stonerivers.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * https://leetcode-cn.com/problems/two-sum/description/
+ * Created by ZhangYuanan on 2018/5/1.
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> sumMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer other = sumMap.get(target - nums[i]);
+            if (other != null) {
+                result[0] = other;
+                result[1] = i;
+                return result;
+            }
+            sumMap.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] all = {2, 7, 11, 15};
+        int sum = 9;
+        TwoSum twoSum = new TwoSum();
+        int[] ints = twoSum.twoSum(all, sum);
+        System.out.println(Arrays.toString(ints));
+    }
+}
---
+++ src/main/java/me/stonerivers/middle/AddTwoNumbers.java
@@ -0,0 +1,51 @@
+package me.stonerivers.middle;
+
+/**
+ * 不要想着用int或者long作为转换媒介,因为会超过其大小限制
+ * Created by ZhangYuanan on 2018/5/1.
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode now1 = l1;
+        ListNode now2 = l2;
+
+        int carry = 0;
+
+        ListNode result = null;
+        ListNode nodeLast = null;
+        while (now1 != null || now2 != null || carry !=0) {
+            int val1 = 0;
+            int val2 = 0;
+            if (now1 != null) {
+                val1 = now1.val;
+                now1 = now1.next;
+            }
+            if (now2 != null) {
+                val2 = now2.val;
+                now2 = now2.next;
+            }
+
+            int sum = val2 + val1 + carry;
+            carry = sum >= 10 ? 1 : 0;
+            ListNode nodeNow = new ListNode(sum % 10);
+            if (result == null) {
+                result = nodeNow;
+            } else {
+                nodeLast.next = nodeNow;
+            }
+            nodeLast = nodeNow;
+        }
+        return result;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
